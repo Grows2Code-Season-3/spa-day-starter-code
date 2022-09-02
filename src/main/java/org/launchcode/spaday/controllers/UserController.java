1c071c710713a2ec95cc7d41cab72688630ab291
Chris Bay <chris@chrisbay.net>
2022-09-02 11:10:48 -0500

    finishes studio

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,31 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping("")
+    public String processAddUserForm(Model model, @ModelAttribute User user, @RequestParam String verify) {
+
+        if (verify.equals(user.getPassword())) {
+            model.addAttribute("username", user.getUsername());
+            return "user/index";
+        }
+
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("error", "Passwords do not match");
+        return "user/add";
+    }
+
+}
